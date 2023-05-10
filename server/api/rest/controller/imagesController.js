3eb9db0e7bfc5357340ffef73750c207e09feab7
Siarhei Paulavets <s.paulavets@gmail.com>
2023-05-10 04:00:43 +0300

    Fix uploads path, make onCatch to getImage

--- server/api/rest/controller/imagesController.js
+++ server/api/rest/controller/imagesController.js
@@ -2,6 +2,7 @@ const gallery = require('../../../model/gallery');
 const path = require('path');
 const BadRequestError = require('../../../errors/BadRequestError')
 const NotFoundError = require("../../../errors/NotFoundError");
+const ApiError = require("../../../errors/ApiError");
 
 async function getImages(req, res, next) {
     try {
@@ -13,17 +14,20 @@ async function getImages(req, res, next) {
 }
 
 async function getImage(req, res, next) {
+    const onCatch = (e) => {
+        if (e) next(new NotFoundError("Изображение не найдено"))
+    };
     try {
         const {uuid, name, extension} = await gallery.getImage(req.userId, req.params.id);
-        const filePath = path.resolve(__dirname + '../../../uploads/' + uuid);
+        const filePath = path.resolve(__dirname + '/../../../uploads/' + uuid);
         const fileName = name + '.' + extension;
         if (req["Content-Disposition"] === "inline")
-            res.sendFile(filePath);
+            res.sendFile(filePath, onCatch);
         else
-            res.download(filePath, fileName)
+            res.download(filePath, fileName, onCatch);
     } catch (e) {
-        if (e instanceof TypeError) e = new NotFoundError("Изображение не найдено");
-        next(e);
+        if (e instanceof ApiError) next(e);
+        else onCatch(e);
     }
 }
 
